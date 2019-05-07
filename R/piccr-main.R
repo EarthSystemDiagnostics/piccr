1f38ad38532ed08b99ca5df858827d8bb717d147
Thomas Münch <tmuench@awi.de>
2019-05-07 11:46:31 +0200

    Turned former main script into main pkg function.

--- R/piccr-main.R
+++ R/piccr-main.R
@@ -1,99 +1,89 @@
-#!/usr/bin/env Rscript
-
 ##===================================================================================
 ## MAIN FUNCTION OF piccr
 ##
 ## author: Thomas Muench (tmuench@awi.de)
-## last update: December 2017
 ##===================================================================================
 
-cat('\n=== Starting piccr ===\n')
+##' "piccr" main routine
+##'
+##' Correct and calibrate raw Picarro CRDS data.
+##' @param dir the directory path as a character string where the raw Picarro
+##' data files and the meta information file are located.
+##' @return \code{0} returned invisibly for a successful programme run.
+##' @author Thomas Münch
+##' @export
+piccr <- function(dir = getwd()) {
+
+    cat("\n=== Starting piccr ===\n")
 
 
-#====================================================================================
-# SOURCE SUB ROUTINES
+    #===========================================================================
+    # ERROR CHECKING
 
-cat('\nSourcing piccr files...\n')
+    if (!dir.exists(dir)) stop("Given directory not found.")
 
-## path of folder containing piccr
-package_folder=as.character(Sys.getenv()['PWD'])
+    file <- file.path(dir, "INPUT.txt")
+    if (!file.exists(file)) {
+        stop(sprintf("Mandatory file INPUT.txt not found in %s."), dir)
+    } else {
+        if (file.access(file) != 0) stop("INPUT.txt not readable.")
+    }
 
-## all R files in piccr folder
-pck.files=list.files(package_folder,pattern='[.][R]$')
-## exclude main routine (does not need to be sourced)
-pck.files=pck.files[-grep('main',pck.files)]
-## source files
-for (f in pck.files){
-    cat(f,':')
-    source(file.path(package_folder,f))
-    cat(' ok.\n')
+
+    #===========================================================================
+    # META INPUT
+
+    cat("Reading meta input...")
+
+    ## read meta input
+    meta.info_folder <- get.input_folder(dir)
+
+    cat("done.\n")
+
+
+    #===========================================================================
+    # READ RAW DATA FROM FILES
+
+    cat("Reading data...")
+
+    ## call reading routine
+    isotope_data <- wrapper.ReadDataFiles(meta.info_folder)
+
+    ## store file data and meta information as list structure
+    folder.iso_data <- list(isotope_data = isotope_data,
+                         meta.info_folder = meta.info_folder)
+
+    cat("done.\n")
+
+
+    #===========================================================================
+    # CORRECT AND CALIBRATE RAW DATA
+
+    cat("Calibrating data...")
+
+    calibration.results <- CalibratePicarroData(
+        folder.iso_data,
+        meta.info_folder$flag.MemCorr,
+        meta.info_folder$flag.Calibration)
+
+    cat("done.\n")
+
+
+    #===========================================================================
+    # OUTPUT OF CALIBRATED DATA AND ACCURACY ESTIMATES
+
+    cat("Writing output...")
+
+    WriteOutput(calibration.results, meta.info_folder)
+
+    cat("done.\n")
+
+
+    #===========================================================================
+    # END
+
+    cat("\nProgramme run successful.\n\n")
+
+    invisible(0)
+
 }
-
-cat('\n')
-
-
-#====================================================================================
-# INPUT
-
-cat('Reading meta input...')
-
-## get command line arguments
-args=commandArgs(TRUE)
-## first command line argument is working folder where Picarro isotope data
-## files are located together with INPUT.txt specifying necessary input info
-working_folder=args[1]
-
-## error handling
-if (is.na(working_folder))
-    stop('\nSpecify folder containing the Picarro data as command-line argument.\n')
-
-## read meta input
-meta.info_folder=get.input_folder(working_folder)
-
-cat('done.\n')
-
-
-#====================================================================================
-# READ RAW DATA FROM FILES
-
-cat('Reading data...')
-
-## call reading routine
-isotope_data=wrapper.ReadDataFiles(meta.info_folder)
-
-## store file data and meta information as list structure
-folder.iso_data=list(isotope_data=isotope_data,
-    meta.info_folder=meta.info_folder)
-
-cat('done.\n')
-
-
-#====================================================================================
-# CORRECT AND CALIBRATE RAW DATA
-
-cat('Calibrating data...')
-
-calibration.results=CalibratePicarroData(folder.iso_data,
-    meta.info_folder$flag.MemCorr,meta.info_folder$flag.Calibration)
-
-cat('done.\n')
-
-
-#====================================================================================
-# OUTPUT OF CALIBRATED DATA AND ACCURACY ESTIMATES
-
-cat('Writing output...')
-
-WriteOutput(calibration.results,meta.info_folder)
-
-cat('done.\n')
-
-
-#====================================================================================
-# END
-
-cat('\nProgramme run successful.\n\n')
-q(save="no",status=0)
-
-
-#====================================================================================
