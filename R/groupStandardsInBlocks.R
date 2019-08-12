e136745496545e36fa989ea98dc0b6fe9907ca6d
twollnik <tom.wollnik@student.hpi.de>
2019-08-12 16:40:52 +0200

    fix indentation

--- R/groupStandardsInBlocks.R
+++ R/groupStandardsInBlocks.R
@@ -14,8 +14,13 @@ groupStandardsInBlocksForDataset <- function(dataset, config){
   for(row in 1:nrow(dataset)){
     id1 = dataset[row, "Identifier 1"]
     if(isStandard(id1, config)){
-      if(inBlock) dataset[row, "block"] = currBlock
-      else currBlock <- currBlock + 1; inBlock <- TRUE; dataset[row, "block"] = currBlock
+      if(inBlock){
+        dataset[row, "block"] <- currBlock
+      } else {
+        currBlock <- currBlock + 1
+        inBlock <- TRUE
+        dataset[row, "block"] <- currBlock
+      }
     } else {
       dataset[row, "block"] = NA
       inBlock <- FALSE
