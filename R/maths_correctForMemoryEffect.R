c0aa58e1cbc38565611b25409196a946b6f37cce
twollnik <tom.wollnik@student.hpi.de>
2019-08-15 15:47:52 +0200

    test memory coefficient calculation

--- R/maths_correctForMemoryEffect.R
+++ R/maths_correctForMemoryEffect.R
@@ -6,6 +6,12 @@ correctForMemoryEffect <- function(datasets, config){
 
 correctSingleDatasetForMemoryEffect <- function(dataset, config){
   
+  
+}
+
+
+calculateMemoryCoefficients <- function(dataset) {
+  
   block1 <- filter(dataset, block == 1)
   
   lastInjectionForEachStandard <- block1 %>% 
@@ -14,12 +20,16 @@ correctSingleDatasetForMemoryEffect <- function(dataset, config){
     select(`Identifier 1` = `Identifier 1`, deltaTrue = `d(18_16)Mean`)
   
   previousStandards <- lastInjectionForEachStandard %>% 
-    .$deltaFromLastInjection %>% 
+    .$deltaTrue %>% 
     lag() %>%
     add_column(lastInjectionForEachStandard, deltaTruePrev = .)
   
-  memoryCorrectedDataset <- block1 %>% 
+  memoryCoefficients <- block1 %>% 
     inner_join(previousStandards) %>%
-    mutate(`d(18_16)Mean` = (`d(18_16)Mean` - deltaTruePrev) / (deltaTruePrev - deltaTrue))
+    mutate(memoryCoefficient = (`d(18_16)Mean` - deltaTruePrev) / (deltaTrue - deltaTruePrev)) %>%
+    drop_na() %>%
+    group_by(`Inj Nr`) %>%
+    summarise(mean = mean(memoryCoefficient))
   
+  return(memoryCoefficients)
 }
---
+++ tests/testthat/testCorrectForMemoryEffect.R
@@ -0,0 +1,69 @@
+library(testthat)
+library(tidyverse)
+
+context("Test the memory correction logic")
+
+test_that("test memory coefficient calculation (dataset contains only block 1 standards)", {
+  
+  # in this dataset: m1 = 0.5, m2 = 0.75, m3 = 1
+  dataset <- tribble(
+    ~`Identifier 1`, ~`Inj Nr`, ~`d(18_16)Mean`, ~block,
+    # ------------ / -------- / -------------- / -------
+      "A",           1,         0.5,             1,
+      "A",           2,         0.75,            1,
+      "A",           3,         1,               1,
+      "B",           1,         1.5,             1,
+      "B",           2,         1.75,            1,
+      "B",           3,         2,               1,
+      "C",           1,         2.5,             1,
+      "C",           2,         2.75,            1,
+      "C",           3,         3,               1
+  )
+  
+  expected <- tribble(
+    ~`Inj Nr`, ~mean,
+    # ------ / ------
+    1,         0.5,
+    2,         0.75, 
+    3,         1
+  )
+  
+  actual <- calculateMemoryCoefficients(dataset)
+  
+  expect_true(all_equal(expected, actual))
+})
+
+test_that("test memory coefficient calculation (dataset contains other blocks and probes)", {
+  
+  # in this dataset: m1 = 0.5, m2 = 1
+  dataset <- tribble(
+    ~`Identifier 1`, ~`Inj Nr`, ~`d(18_16)Mean`, ~block,
+    # ------------ / -------- / -------------- / -------
+    "Probe1",        1,         10,              NA,
+    "Probe1",        2,         10,              NA,
+    "A",             1,         0.5,             1,
+    "A",             2,         1,               1,
+    "B",             1,         1.5,             1,
+    "B",             2,         2,               1,
+    "C",             1,         2.5,             1,
+    "C",             2,         3,               1,
+    "Probe2",        1,         10,              NA,
+    "Probe2",        2,         10,              NA,
+    "A",             1,         20,              2,
+    "A",             2,         20,              2,
+    "B",             1,         0,               2,
+    "B",             2,         0,               2,
+    "Probe3",        2,         10,              NA
+  )
+  
+  expected <- tribble(
+    ~`Inj Nr`, ~mean,
+    # ------ / ------
+    1,         0.5,
+    2,         1
+  )
+  
+  actual <- calculateMemoryCoefficients(dataset)
+  
+  expect_true(all_equal(expected, actual))
+})
