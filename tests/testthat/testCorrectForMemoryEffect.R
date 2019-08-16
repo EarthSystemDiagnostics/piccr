25063570da09d7655a5de3bbd8afd3946102003e
twollnik <tom.wollnik@student.hpi.de>
2019-08-16 09:17:34 +0200

    fix memory effect tests

--- tests/testthat/testCorrectForMemoryEffect.R
+++ tests/testthat/testCorrectForMemoryEffect.R
@@ -47,26 +47,6 @@ test_that("test memory coefficient calculation (dataset contains other blocks an
     # ------------ / -------- / -------------- / -------
     "Probe1",        1,         10,              NA,
     "Probe1",        2,         10,              NA,
-    "A",             1,         0.5,             1,
-    "A",             2,         1,               1,
-    "B",             1,         1.5,             1,
-    "B",             2,         2,               1,
-    "C",             1,         2.5,             1,
-    "C",             2,         3,               1,
-    "Probe2",        1,         10,              NA,
-    "Probe2",        2,         10,              NA,
-    "A",             1,         20,              2,
-    "A",             2,         20,              2,
-    "B",             1,         0,               2,
-    "B",             2,         0,               2,
-    "Probe3",        2,         10,              NA
-  )
-  
-  expected <- tribble(
-    ~`Identifier 1`, ~`Inj Nr`, ~`d(18_16)Mean`, ~block,
-    # ------------ / -------- / -------------- / -------
-    "Probe1",        1,         10,              NA,
-    "Probe1",        2,         10,              NA,
     "A",             1,         5.5,             1,
     "A",             2,         1,               1,
     "B",             1,         1.5,             1,
@@ -79,7 +59,27 @@ test_that("test memory coefficient calculation (dataset contains other blocks an
     "A",             2,         20,              2,
     "B",             1,         10,              2,
     "B",             2,         0,               2,
-    "Probe3",        1,         5,              NA
+    "Probe3",        1,         10,              NA
+  )
+  
+  expected <- tribble(
+    ~`Identifier 1`, ~`Inj Nr`, ~`d(18_16)Mean`, ~block,
+    # ------------ / -------- / -------------- / -------
+    "Probe1",        1,         10,              NA,
+    "Probe1",        2,         10,              NA,
+    "A",             1,         1,               1,
+    "A",             2,         1,               1,
+    "B",             1,         2,               1,
+    "B",             2,         2,               1,
+    "C",             1,         3,               1,
+    "C",             2,         3,               1,
+    "Probe2",        1,         10,              NA,
+    "Probe2",        2,         10,              NA,
+    "A",             1,         20,              2,
+    "A",             2,         20,              2,
+    "B",             1,         0,               2,
+    "B",             2,         0,               2,
+    "Probe3",        1,         20,              NA
   )
   
   actual <- correctSingleDatasetForMemoryEffect(dataset)
