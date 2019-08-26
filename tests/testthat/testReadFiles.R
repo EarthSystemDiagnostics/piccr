91d39ace277d7a92805bd478289b407db8c999a0
twollnik <tom.wollnik@student.hpi.de>
2019-08-26 09:54:27 +0200

    test reading data with spaces

--- tests/testthat/testReadFiles.R
+++ tests/testthat/testReadFiles.R
@@ -16,9 +16,9 @@ test_that("test content of files read is correct", {
   data <- readFiles(config)
   
   expect_equal(data$HIDS2041_IsoWater_20151125_111138.csv, 
-                        read_csv("test_data/HIDS2041_IsoWater_20151125_111138.csv"))
+                        read_csv("test_data/no_spaces/HIDS2041_IsoWater_20151125_111138.csv"))
   expect_equal(data$HIDS2041_IsoWater_20151126_115726.csv, 
-                        read_csv("test_data/HIDS2041_IsoWater_20151126_115726.csv"))
+                        read_csv("test_data/no_spaces/HIDS2041_IsoWater_20151126_115726.csv"))
   expect_equal(data$HIDS2041_IsoWater_20151127_143940.csv, 
-                        read_csv("test_data/HIDS2041_IsoWater_20151127_143940.csv"))
+                        read_csv("test_data/no_spaces/HIDS2041_IsoWater_20151127_143940.csv"))
 })
