0dcfcaab036813eb9eef707f64238ce7342ecc99
twollnik <tom.wollnik@student.hpi.de>
2019-09-06 15:19:31 +0200

    add docstrings for public functions

--- R/main_processData.R
+++ R/main_processData.R
@@ -1,4 +1,40 @@
+#' processData
+#'
+#' The main interface for using piccr as a part of other programs. 
+#' 
+#' Implements the processing of water isotope data using memory correction,
+#' calibration, and drift correction.
+#' Uses the processing options set in the parameter 'config'.
+#' Calculates quality indicators for each dataset such as delta excess and the pooled 
+#' standard deviation.
+#' 
+#' @param datasets A named list of data.frames. The isotope measurement data
+#'                 as output by the Picarro device.
+#' @param config A named list. Required fiels: 
+#'                   $average_over_last_n_inj (a number or "all")
+#'                   $use_three_point_calibration (logical)
+#'                   $calibration_method (1, 2, or 3)
+#'                   $use_memory_correction (logical)
+#'                   $standards (named list of $name (character), $o18_True (numerical), 
+#'                               $H2_True (numerical), $use_for_drift_correction (logical), 
+#'                               $use_for_calibration (logical))
+#'
+#' @return A named, nested list. List structure:
+#'             $memoryCorrected (list of lists, the names are the names 
+#'                               of the input parameter 'datasets')
+#'                 $datasetMemoryCorrected (data.frame)
+#'                 $memoryCoefficients (data.frame)
+#'             $calibrated (list of data.frames, the names are the names 
+#'                          of the input parameter 'datasets'))
+#'             $processed (list of data.frames, the names are the names 
+#'                         of the input parameter 'datasets'))
+#'             $pooledStdDev (list of lists, the names are the names 
+#'                            of the input parameter 'datasets'))
+#'                 $d18O (numerical)
+#'                 $dD (numerical)
+#' 
 #' @export
+#'
 processData <- function(datasets, config){
   
   datasets <- groupStandardsInBlocks(datasets, config) %>%
--- R/main_processFiles.R
+++ R/main_processFiles.R
@@ -1,6 +1,19 @@
 library(magrittr)
 
+#' processFiles
+#' 
+#' Main interface for the stand-alone use of piccr. Input the path to
+#' a config file. Then process measurement data according to the 
+#' configurations in the config file.
+#' 
+#' Relies on processData(..) to do the actual processing.
+#' 
+#' @param configFile A string. The path to a config file.
+#'
+#' @return A list. See the documentation for processData(..) for details.
+#' 
 #' @export
+#' 
 processFiles <- function(configFile){
   
   config <- parseConfig(configFile)
